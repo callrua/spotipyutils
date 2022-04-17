e3b77c808a9814ac21673cf4c5e957ff29e993ee
Ruairi Callaghan <ruairiscallaghan@gmail.com>
2022-04-17 19:02:46 +0100

    Remove prototype

--- utils/commands/cmd_hi.py
+++
@@ -1,9 +0,0 @@
-from utils.cli import pass_environment
-
-import click
-
-@click.command("greet", short_help="Greet me")
-@pass_environment
-def cli(ctx):
-    """" Simple greet """
-    ctx.greet()
